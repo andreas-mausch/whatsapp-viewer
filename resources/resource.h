3462849b1049373f3377498339b9c9132da6d997
neonew <neonew@gmail.com>
2014-03-20 12:28:34 +0100

    Main dialog and menu

--- resources/resource.h
+++ resources/resource.h
@@ -2,16 +2,22 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resources.rc
 //
-#define IDI_ICON1                       103
 #define IDI_ICON                        103
+#define IDD_MAIN                        104
+#define IDR_MENU1                       105
+#define IDR_MENU_MAIN                   105
+#define IDC_MAIN_CHATS                  1001
+#define IDC_MAIN_MESSAGES               1002
+#define ID_FILE_OPEN40001               40001
+#define ID_MENU_MAIN_FILE_OPEN          40002
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40003
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
