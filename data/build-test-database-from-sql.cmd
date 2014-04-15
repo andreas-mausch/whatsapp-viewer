2fde9ddf96bbe46be98978e5292db742f1638cff
neonew <neonew@gmail.com>
2014-04-16 01:49:30 +0200

    updated test data

--- data/build-test-database-from-sql.cmd
+++ data/build-test-database-from-sql.cmd
@@ -1,1 +1,9 @@
+@echo off
+
+del test-database.db
+del test-database-large.db
+
+echo Building test-database.db...
 sqlite3 test-database.db < test-database.sql
+echo Building test-database-large.db...
+sqlite3 test-database-large.db < test-database-large.sql
