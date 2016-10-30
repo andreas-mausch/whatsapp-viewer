c51a912380e1a0f6ef7297bcfcb0e13bf3ab8908
neonew <neonew@gmail.com>
2016-10-30 19:12:06 +0100

    empty electron app

---
+++ main.js
@@ -0,0 +1,15 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+});
