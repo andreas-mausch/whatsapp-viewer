ffa072135d5b4ba6da535ba350701909ae61d149
neonew <neonew@gmail.com>
2016-11-04 21:23:13 +0100

    build message object with more fields than just data: fromMe, location, media, timestamp; added moment.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var sqlite3 = require('sqlite3').verbose();
 // this is discouraged however there doesn't seem to be a solution for npm without webpack/browserify
 // https://vuejs.org/guide/installation.html
 var Vue = require('vue/dist/vue.js');
+var moment = require('moment');
 
 var database = null;
 
@@ -33,7 +34,37 @@ var app = new Vue({
             ORDER BY timestamp ASC`,
         key,
         function(error, row) {
-          messages.push(row.data);
+          var message = {
+            data: row.data,
+            fromMe: row.key_from_me,
+            status: row.status,
+            timestamp: moment(row.timestamp),
+            remoteResource: row.remote_resource,
+            rawData: row.raw_data
+          };
+
+          if (row.latitude != 0 || row.longitude != 0) {
+            message.location = {
+              latitude: row.latitude,
+              longitude: row.longitude
+            };
+          }
+
+          if (row.media_url || row.media_mime_type || row.media_wa_type != 0 ||
+            row.media_size != 0 || row.media_name || row.media_duration != 0 || row.thumb_image) {
+            message.media = {
+              url: row.media_url,
+              mimeType: row.media_mime_type,
+              whatsappType: row.media_wa_type,
+              size: row.media_size,
+              name: row.media_name,
+              duration: row.media_duration,
+              thumbImage: row.thumb_image
+            };
+          }
+
+
+          messages.push(message);
         });
     }
   }
