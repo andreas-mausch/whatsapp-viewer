302ce61c0a6b96774f675914db1480bdbbe8599f
neonew <neonew@gmail.com>
2018-03-27 04:02:12 +0200

    cleaned up unused aes method

--- source/Libraries/AES/MyAES.cpp
+++ source/Libraries/AES/MyAES.cpp
@@ -3,19 +3,6 @@
 
 int aesBlocksize = 16;
 
-void encrypt_aes_cbc_192(const unsigned char *input, unsigned char *output, int length, const unsigned char *key, unsigned char *initVector)
-{
-	mbedtls_aes_context context;
-
-	mbedtls_aes_init(&context);
-	mbedtls_aes_setkey_enc(&context, key, 192);
-	if (mbedtls_aes_crypt_cbc(&context, MBEDTLS_AES_ENCRYPT, length, initVector, input, output) != 0)
-	{
-		throw Exception("Could not encrypt block");
-	}
-	mbedtls_aes_free(&context);
-}
-
 void decrypt_aes_cbc_192(const unsigned char *input, unsigned char *output, int length, const unsigned char *key, unsigned char *initVector)
 {
 	mbedtls_aes_context context;
--- source/Libraries/AES/MyAES.h
+++ source/Libraries/AES/MyAES.h
@@ -1,6 +1,5 @@
 #pragma once
 
-void encrypt_aes_cbc_192(const unsigned char *input, unsigned char *output, int length, const unsigned char *key, unsigned char *initVector);
 void decrypt_aes_cbc_192(const unsigned char *input, unsigned char *output, int length, const unsigned char *key, unsigned char *initVector);
 void decrypt_aes_cbc_256(const unsigned char *input, unsigned char *output, int length, const unsigned char *key, unsigned char *initVector);
 
