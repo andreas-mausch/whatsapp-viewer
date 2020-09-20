cb61a7cf6e192bfdaae4233fd4d37093523b80b8
neonew <neonew@gmail.com>
2020-09-20 21:43:07 +0200

    #46 Don't export private functions

--- source/WhatsApp/Crypt5.h
+++ source/WhatsApp/Crypt5.h
@@ -1,6 +1,5 @@
 #pragma once
 
-void buildKey(unsigned char *key, const std::string &accountName);
 void decryptWhatsappDatabase5(const std::string &filename, const std::string &filenameDecrypted, unsigned char *key);
 void decryptWhatsappDatabase5(const std::string &filename, const std::string &filenameDecrypted, const std::string &accountName);
 
